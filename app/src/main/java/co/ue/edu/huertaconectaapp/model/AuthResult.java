08a6713ec3bdba711cef67d3dd40f8f87e1d6600
ivan <ivansierra259@gmail.com>
2026-04-10 21:11:00 -0500

    toma sqlite, modelos de db, token en db,creacion de huertas y asignacion al usuario

--- app/src/main/java/co/ue/edu/huertaconectaapp/model/AuthResult.java
+++ app/src/main/java/co/ue/edu/huertaconectaapp/model/AuthResult.java
@@ -1,9 +1,16 @@
 package co.ue.edu.huertaconectaapp.model;
 
 public class AuthResult {
-    private String message, token;
-    public String getMessage() {return message;}
-    public void setMessage(String message) {this.message = message;}
-    public String getToken() {return token;}
-    public void setToken(String token) {this.token = token;}
+    private String message;
+    private String token;
+    private UserResponse user;
+
+    public String getMessage() { return message; }
+    public void setMessage(String message) { this.message = message; }
+
+    public String getToken() { return token; }
+    public void setToken(String token) { this.token = token; }
+
+    public UserResponse getUser() { return user; }
+    public void setUser(UserResponse user) { this.user = user; }
 }
